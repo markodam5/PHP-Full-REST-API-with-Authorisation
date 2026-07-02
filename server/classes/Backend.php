6dda180fe45c6586ee33792adcdb70b347241ec5
Marko Damjanac <36675797+markodam5@users.noreply.github.com>
2026-07-02 11:00:56 +0200

    Update require statements to use BASE_PATH

--- server/classes/Backend.php
+++ server/classes/Backend.php
@@ -1,6 +1,6 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . "/rest/server/config/config.php");
-require_once($_SERVER['DOCUMENT_ROOT'] . "/rest/server/javascript_helper.php");
+require_once BASE_PATH .'/server/config/config.php';
+require_once BASE_PATH .'/server/helper.php';
 
 class Backend{
     protected $db;
