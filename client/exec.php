4b83146cb6157f99059d8e1f24077645d8565b4b
Marko Damjanac <36675797+markodam5@users.noreply.github.com>
2026-07-04 11:11:02 +0200

    Clean up navigation and sidebar content
    Removed unused navigation links and content from the navbar and main section.

--- client/exec.php
+++ client/exec.php
@@ -100,35 +100,14 @@ require_once "RestClient.php";
                 </button>
                 <a class="navbar-brand" href="#">Rest project</a>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
+            
         </div> <!-- /.container -->
     </nav>
 
     <!-- Page Content -->
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
-                <p class="lead">REST API</p>
-                <div class="list-group">
-                    <a href="#" class="list-group-item">Category 1</a>
-                    <a href="#" class="list-group-item">Category 2</a>
-                    <a href="#" class="list-group-item">Category 3</a>
-                </div>
-            </div>
-
+            
             <div class="col-md-9">
                 <div class="row">
                     <?php
