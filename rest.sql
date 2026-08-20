e2ba3338f47246fb7ea3feb7ff56633301042c0c
Marko Damjanac <36675797+markodam5@users.noreply.github.com>
2026-08-20 13:49:07 +0200

    Update movie entries in rest.sql

--- rest.sql
+++ rest.sql
@@ -40,14 +40,18 @@ CREATE TABLE `movies` (
 --
 
 INSERT INTO `movies` (`movie_id`, `movie_title`, `movie_picture`, `movie_link`, `movie_price`) VALUES
-(1, 'Force 2', 'http://localhost/rest/server/images/force2.jpg', 'https://www.youtube.com/watch?v=XAp78FqTfw8', 50),
-(2, 'Mechanic', 'http://localhost/rest/server/images/mechanic.jpg', 'https://www.youtube.com/watch?v=CMklQNn0OH0', 60),
-(3, 'Rambo 4', 'http://localhost/rest/server/images/rambo4.jpg', 'https://www.youtube.com/watch?v=CPx-0qDRroU', 1500),
-(4, 'Robocop', 'http://localhost/rest/server/images/robocop.jpg', 'https://www.youtube.com/watch?v=Z931XZ2wfpE', 1000),
-(5, 'Swiss Army Man', 'http://localhost/rest/server/images/swiss-army.jpg', 'https://www.youtube.com/watch?v=yrK1f4TsQfM', 1100),
-(6, 'Transformers: The Last Knight', 'http://localhost/rest/server/images/transformers.jpg', 'https://www.youtube.com/watch?v=AntcyqJ6brc', 900),
-(7, 'Warcraft: The beginning', 'http://localhost/rest/server/images/warcraft.jpg', 'https://www.youtube.com/watch?v=RVzb956kdgc', 900),
-(8, 'X-Men: Apocalypse', 'http://localhost/rest/server/images/x-men.jpg', 'https://www.youtube.com/watch?v=COvnHv42T-A', 900);
+(1, 'Avatar', 'http://localhost/rest/server/images/avatar.jpg', 'https://www.youtube.com/watch?v=5PSNL1qE6VY', 1200),
+(2, 'Titanic', 'http://localhost/rest/server/images/titanic.jpg', 'https://www.youtube.com/watch?v=kVrqfYjk5nA', 1100),
+(3, 'Avengers: Endgame', 'http://localhost/rest/server/images/avengers-endgame.jpg', 'https://www.youtube.com/watch?v=TcMBFSGVi1c', 1500),
+(4, 'Spider-Man: No Way Home', 'http://localhost/rest/server/images/spider-man.jpg', 'https://www.youtube.com/watch?v=JfVOs4VSpmA', 1400),
+(5, 'The Godfather', 'http://localhost/rest/server/images/godfather.jpg', 'https://www.youtube.com/watch?v=UaVTIH8mujA', 1300),
+(6, 'Pulp Fiction', 'http://localhost/rest/server/images/pulp-fiction.jpg', 'https://www.youtube.com/watch?v=s7EdQ4FqbhY', 1250),
+(7, 'Fight Club', 'http://localhost/rest/server/images/fight-club.jpg', 'https://www.youtube.com/watch?v=qtRKdVHc-cE', 1150),
+(8, 'Forrest Gump', 'http://localhost/rest/server/images/forrest-gump.jpg', 'https://www.youtube.com/watch?v=bLvqoHBptjg', 1000),
+(9, 'The Lord of the Rings: The Return of the King', 'http://localhost/rest/server/images/lotr-return.jpg', 'https://www.youtube.com/watch?v=r5X-hFf6Bwo', 1500),
+(10, 'Parasite', 'http://localhost/rest/server/images/parasite.jpg', 'https://www.youtube.com/watch?v=5xH0HfJHsaY', 1200),
+(11, 'Toy Story', 'http://localhost/rest/server/images/toy-story.jpg', 'https://www.youtube.com/watch?v=v-PjgYDrg70', 900),
+(12, 'Avatar: The Way of Water', 'http://localhost/rest/server/images/avatar-way-of-water.jpg', 'https://www.youtube.com/watch?v=d9MyW72ELq0', 1350);
 
 -- --------------------------------------------------------
 
